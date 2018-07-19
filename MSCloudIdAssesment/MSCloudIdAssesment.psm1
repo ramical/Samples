05b8ea724f1cdae3f81d046e2ec21db5db28602d
Ramiro Calderon <ramical@ntdev.microsoft.com>
2018-07-19 16:43:18 -0700

    Add function for application key report

--- MSCloudIdAssesment/MSCloudIdAssesment.psm1
+++ MSCloudIdAssesment/MSCloudIdAssesment.psm1
@@ -32,6 +32,10 @@
 #>
 function Start-MSCloudIdSession		
 {
+    [CmdletBinding()]
+    param
+    ()
+
     Connect-MsolService
     Connect-AzureAD
 }
@@ -167,6 +171,97 @@ function Get-MSCloudIdAdminRolesReport
         }
     }
 }
+
+<# 
+ .Synopsis
+  Provides a report to show all the keys expiration date accross application and service principals 
+
+ .Description
+  Provides a report to show all the keys expiration date accross application and service principals
+  
+ .Example
+  Connect-AzureAD
+  Get-MSCloudIdApplicationKeyExpirationReport
+  
+#>
+Function Get-MSCloudIdApplicationKeyExpirationReport
+{
+    param()
+    
+    $apps = Get-AzureADApplication -All $true
+
+    foreach($app in $apps)
+    {
+        $appObjectId = $app.ObjectId
+        $appName = $app.DisplayName
+        
+
+        $appKeys = Get-AzureADApplicationKeyCredential -ObjectId $appObjectId
+
+        foreach($appKey in $appKeys)
+        {        
+            $result = New-Object PSObject
+            $result  | Add-Member -MemberType NoteProperty -Name "Display Name" -Value $appName
+            $result  | Add-Member -MemberType NoteProperty -Name "Object Type" -Value "Application"
+            $result  | Add-Member -MemberType NoteProperty -Name "KeyType" -Value $appKey.Type
+            $result  | Add-Member -MemberType NoteProperty -Name "Start Date" -Value $appKey.StartDate
+            $result  | Add-Member -MemberType NoteProperty -Name "End Date" -Value $appKey.EndDate
+            $result  | Add-Member -MemberType NoteProperty -Name "Usage" -Value $appKey.Usage
+            Write-Output $result
+        }
+
+        $appKeys = Get-AzureADApplicationPasswordCredential -ObjectId $appObjectId
+        
+        foreach($appKey in $app.PasswordCredentials)
+        {        
+            $result = New-Object PSObject
+            $result  | Add-Member -MemberType NoteProperty -Name "Display Name" -Value $appName
+            $result  | Add-Member -MemberType NoteProperty -Name "Object Type" -Value "Application"
+            $result  | Add-Member -MemberType NoteProperty -Name "KeyType" -Value "Password"
+            $result  | Add-Member -MemberType NoteProperty -Name "Start Date" -Value $appKey.StartDate
+            $result  | Add-Member -MemberType NoteProperty -Name "End Date" -Value $appKey.EndDate
+            Write-Output $result
+        }
+    }
+
+    
+    $servicePrincipals = Get-AzureADServicePrincipal -All $true
+
+    foreach($sp in $servicePrincipals)
+    {
+        $spName = $sp.DisplayName
+        $spObjectId = $sp.ObjectId
+
+        $spKeys = Get-AzureADServicePrincipalKeyCredential -ObjectId $spObjectId        
+
+        foreach($spKey in $spKeys)
+        {
+            $result = New-Object PSObject
+            $result  | Add-Member -MemberType NoteProperty -Name "Display Name" -Value $spName
+            $result  | Add-Member -MemberType NoteProperty -Name "Object Type" -Value "Service Principal"
+            $result  | Add-Member -MemberType NoteProperty -Name "KeyType" -Value $spKey.Type
+            $result  | Add-Member -MemberType NoteProperty -Name "Start Date" -Value $spKey.StartDate
+            $result  | Add-Member -MemberType NoteProperty -Name "End Date" -Value $spKey.EndDate
+            $result  | Add-Member -MemberType NoteProperty -Name "Usage" -Value $spKey.Usage
+            Write-Output $result
+        }    
+        
+        $spKeys = Get-AzureADServicePrincipalPasswordCredential -ObjectId $spObjectId    
+
+        
+        foreach($spKey in $spKeys)
+        {
+            $result = New-Object PSObject
+            $result  | Add-Member -MemberType NoteProperty -Name "Display Name" -Value $spName
+            $result  | Add-Member -MemberType NoteProperty -Name "Object Type" -Value "Service Principal"
+            $result  | Add-Member -MemberType NoteProperty -Name "KeyType" -Value "Password"
+            $result  | Add-Member -MemberType NoteProperty -Name "Start Date" -Value $spKey.StartDate
+            $result  | Add-Member -MemberType NoteProperty -Name "End Date" -Value $spKey.EndDate
+            Write-Output $result
+        }    
+    }
+}
+
 
 <# 
  .Synopsis
@@ -435,5 +530,6 @@ Export-ModuleMember -Function Get-MSCloudIdNotificationEmailAddresses
 Export-ModuleMember -Function Get-MSCloudIdAppAssignmentReport
 Export-ModuleMember -Function Get-MSCloudIdAdminRolesReport
 Export-ModuleMember -Function Get-MSCloudIdConsentGrantList
+Export-ModuleMember -Function Get-MSCloudIdApplicationKeyExpirationReport
 Export-ModuleMember -Function Get-MSCloudIdADFSEndpoints
 Export-ModuleMember -Function Export-MSCloudIdADFSConfiguration
